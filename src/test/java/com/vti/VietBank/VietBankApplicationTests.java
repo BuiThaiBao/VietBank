5c777adf45d9e0667cfc4952db6cf27b1399e2a9
buith <buithaibao2k4@gmail.com>
2025-09-17 22:15:02 +0700

    Connect Git

---
+++ src/test/java/com/vti/VietBank/VietBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.VietBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
